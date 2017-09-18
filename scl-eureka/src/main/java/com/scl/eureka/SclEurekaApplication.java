2e7129259ca6a9303a91dcfdb7a55fcda491241b
盛承龙 <shengchenglong@fulan.com.cn>
2017-09-18 14:49:00 +0800

    add eureka

---
+++ scl-eureka/src/main/java/com/scl/eureka/SclEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.scl.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SclEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SclEurekaApplication.class, args);
+	}
+}
